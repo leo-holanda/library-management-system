a6643e47394f6decbbc8ff2841863c269bcd0d88
Leonardo Ulisses <leoholaanda2@gmail.com>
2018-07-26 13:32:52 -0300

    creating check functions

--- src/functions.c
+++ src/functions.c
@@ -1,12 +1,13 @@
 #include <stdio.h> 
 #include <stdlib.h>
+#include <string.h>
 #include "functions.h"
 
 typedef struct book
 {
 	int id;
 	char author_name[50];
-	char book_name[50];
+	char book_title[50];
 	char publisher[25];
 } book;
 
@@ -39,16 +40,16 @@ void addBook()
 
 	book new_book;
 	printf("Digite o nome do livro: ");
-	fgets(new_book.book_name,50,stdin);
+	fgets(new_book.book_title,50,stdin);
 	printf("Digite o nome do autor: ");
 	fgets(new_book.author_name,50,stdin);
 	printf("Digite o nome da editora: ");
 	fgets(new_book.publisher,25,stdin);
 
 	FILE *f = fopen("books//data.txt","a+");
-	fprintf(f,"%s",new_book.book_name);
+	fprintf(f,"\n%s",new_book.book_title);
 	fprintf(f,"%s",new_book.author_name);
-	fprintf(f,"%s\n",new_book.publisher);
+	fprintf(f,"%s",new_book.publisher);
 	fclose(f);
 
 	if (getAnswer(0) == 'S')
@@ -71,23 +72,20 @@ void showAllBooks()
 
 	while(fgets(line,50,f) != NULL)
 	{
-		if (count == 0)
+		if (count == 1)
 		{
 			printf("Título: %s", line);
 		}
-		else if (count == 1)
+		else if (count == 2)
 		{
 			printf("Autor(a): %s", line);
 		}
-		else if (count == 2)
-		{
-			printf("Editora: %s", line);
-		}
 		else if (count == 3)
 		{
+			printf("Editora: %s\n", line);
 			count = -1;
-			printf("%s", line);
 		}
+
 		count++;
 	}
 
@@ -103,17 +101,133 @@ void showAllBooks()
 
 void checkTitle()
 {
+	system("clear");
 
+	char book_title[50];
+	char author_name[50];
+	char publisher[50];
+	char line[50];
+	
+	char searched_title[50];
+	printf("Digite o título que deseja buscar: ");
+	fgets(searched_title,50,stdin);
+	printf("\n");
+
+	FILE *f = fopen("books//data.txt","r");
+
+	while(fgets(line,50,f) != NULL)
+	{
+		fgets(book_title,50,f);
+
+		if (strcmp(searched_title,book_title) ==0 )
+		{
+			fgets(author_name,50,f);
+			fgets(publisher,50,f);
+		
+			printf("Título: %s", book_title);
+			printf("Autor: %s", author_name);
+			printf("Editora: %s\n",publisher);
+		}
+		else
+		{
+			fgets(line,50,f);
+			fgets(line,50,f);
+		}
+	}
+
+	if (getAnswer(2) == 'S')
+	{
+		checkTitle();
+	}
+	else
+	{
+		showMenu();
+	}	
 }
 
 void checkAuthor()
 {
+	system("clear");
 
+	char book_title[50];
+	char author_name[50];
+	char publisher[50];
+	char line[50];
+	
+	char searched_author[50];
+	printf("Digite o nome do autor que deseja buscar: ");
+	fgets(searched_author,50,stdin);
+	printf("\n");
+
+	FILE *f = fopen("books//data.txt","r");
+
+	while(fgets(line,50,f) != NULL)
+	{
+		fgets(book_title,50,f);
+		fgets(author_name,50,f);
+			
+		if (strcmp(searched_author,author_name) ==0 )
+		{
+			fgets(publisher,50,f);
+		
+			printf("Título: %s", book_title);
+			printf("Autor: %s", author_name);
+			printf("Editora: %s\n",publisher);
+		}
+		else
+		{
+			fgets(line,50,f);
+		}
+	}
+
+	if (getAnswer(2) == 'S')
+	{
+		checkAuthor();
+	}
+	else
+	{
+		showMenu();
+	}	
 }
 
 void checkPublisher()
 {
+	system("clear");
 
+	char book_title[50];
+	char author_name[50];
+	char publisher[50];
+	char line[50];
+	
+	char searched_publisher[50];
+	printf("Digite o nome do autor que deseja buscar: ");
+	fgets(searched_publisher,50,stdin);
+	printf("\n");
+
+	FILE *f = fopen("books//data.txt","r");
+
+	while(fgets(line,50,f) != NULL)
+	{
+		fgets(book_title,50,f);
+		fgets(author_name,50,f);
+		fgets(publisher,50,f);	
+		
+		if (strcmp(searched_publisher,publisher) ==0 )
+		{
+			printf("Título: %s", book_title);
+			printf("Autor: %s", author_name);
+			printf("Editora: %s\n",publisher);
+		}
+	}
+
+	if (getAnswer(2) == 'S')
+	{
+		checkPublisher();
+	}
+	else
+	{
+		showMenu();
+	}	
 }
 
 void showMenu()
@@ -149,6 +263,7 @@ void showMenu()
 
 void showCheckMenu()
 {
+	system("clear");
 	printf("Deseja consultar por...\n");
 	printf("1 - Título\n");
 	printf("2 - Autor\n");
